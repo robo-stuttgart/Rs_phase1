3884dfc33f663b08204db4a76750d76b0fabcd9a
Raja <rajasen9903@gmail.com>
2015-12-03 16:57:11 +0100

    Added AI module

--- rs-phase-one/rs-phase-one-main/src/main/java/main/Main.java
+++ rs-phase-one/rs-phase-one-main/src/main/java/main/Main.java
@@ -3,7 +3,28 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		
+		// startup ()
+		//   establish network connection (google ProtoBuf)
+		//   Creater Logger()
+		//   init gui()
+		//   register observers, etc... 
+		//        Observer design pattern (very important!)
+		
+		while (true)
+		{
+			// get Position Data
+			// 	-from network // SSLVisionCam in Sumatra
+			
+			// do analysis // like metis in sumatra
+			
+			// assing roles like roleAssigner in Sumatra
+			
+			// excecute strategy // Roles in Sumatra
+			
+			// move Robots // SkillExecutor
+		}
 	}
 
 }
